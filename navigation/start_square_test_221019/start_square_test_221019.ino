c0a1f4ee6d2cd6e66c850aac4d6b0f8dd5028319
Ruitong Sun <rs2177@cam.ac.uk>
2022-10-20 09:08:44 +0100

    Update start_route + line_follow

--- navigation/start_square_test_221019/start_square_test_221019.ino
+++ navigation/start_square_test_221019/start_square_test_221019.ino
@@ -1,7 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
-#include "functions.h"
 
 /* setup the motor and create the DC motor object*/
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // Create the Adafruit_MotorShield object
@@ -10,103 +9,158 @@ Adafruit_DCMotor *Lwheel = AFMS.getMotor(2);        // LEFT
 
 /*defining pins and variables*/
 
-#define push_pn 2 // switch
-#define l0_pn 3   // left
-#define l1_pn 4   // right
-#define l2_pnn 5  // far right
+#define BUTTON_PIN 2 // switch
+#define l0_pn 3      // left
+#define l1_pn 4      // right
+#define l2_pn 5     // far right
 
 // Variables will change:
-int button_state = HIGH;            // the current reading from the input pin
-int last_button_state = HIGH; // the previous reading from the input pin
 bool flag_started = false;
 bool flag_onoff = false;
-bool push;
 char flag_nav = 'P';
+int motor_speed = 250;
+int l0, l1, l2;
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
-unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
+
+// button variable
+byte lastButtonState = LOW;
+unsigned long debounceDuration = 50; // millis
+unsigned long lastTimeButtonStateChanged = 0;
 
 void setup()
 {
-  pinMode(push_pn, INPUT);
-
-  // set initial state to off
-  flag_onoff = false;
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(l0_pn, INPUT); // light sensor on the left
+  pinMode(l1_pn, INPUT); // light sensor on the right
+  pinMode(l2_pn, INPUT); // light sensor on the far right
   Serial.begin(9600);
+  AFMS.begin(); // Connect to the controller
 }
+
+
 
 void loop()
 {
+  // read and store light sensor values
+  l0 = digitalRead(l0_pn);
+  l1 = digitalRead(l1_pn);
+  l2 = digitalRead(l2_pn);
 
-  // sensor_read(); //--> read push button and store the value into "push"
-  push = digitalRead(push_pn);
-  Serial.print("push: ");
-  Serial.println(push);
-  /**********************************************************************************/
-  /* push button debounce function */
-  // check to see if you just pressed the button
-  // (i.e. the input went from LOW to HIGH), and you've waited long enough
-  // since the last press to ignore any noise:
+  // print values of the light sensors to the serial monitor
+  Serial.print("Sensor Readings: ");
+  Serial.print(l0);
+  Serial.print(l1);
+  Serial.println(l2);
 
-  // If the switch changed, due to noise or pressing:
-  if (push != last_button_state)
+  Serial.print("Flag States: ");
+  Serial.println(flag_nav);
+  
+  
+  //button debounce
+  if (millis() - lastTimeButtonStateChanged > debounceDuration)
   {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-    Serial.println(lastDebounceTime);
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay)
-  {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
-    
-    // if the button state has changed:
-    if (push != button_state)
+    byte buttonState = digitalRead(BUTTON_PIN);
+    if (buttonState != lastButtonState)
     {
-      button_state = push;
-      Serial.print("button_state: ");
-      Serial.println(button_state);
-
-      // only toggle the LED if the new button state is HIGH
-      if (button_state == LOW)
+      lastTimeButtonStateChanged = millis();
+      lastButtonState = buttonState;
+      if (buttonState == LOW)
       {
-        flag_onoff != flag_onoff;
+        // do an action, for example print on Serial
+        Serial.println("Button released");
+        flag_onoff = !flag_onoff;
       }
     }
   }
-
-  // save the reading. Next time through the loop, it'll be the lastbutton_state:
-  last_button_state = push;
-
-  /**********************************************************************************/
-  Serial.print("flag_onoff: ");
+  Serial.print("ONOFF: ");
   Serial.println(flag_onoff);
-  if (flag_onoff == false && flag_nav != 'P')
-  {
-    Lwheel->run(RELEASE);
-    Lwheel->setSpeed(0);
-    flag_nav = 'P';
+
+  if (flag_onoff == true) {
+    line_follow();
   }
-  else if (flag_onoff == true && flag_nav != 'F')
-  {
-    Lwheel->run(FORWARD);
-    Lwheel->setSpeed(150);
-    flag_nav = 'F';
-  }
-  else
-  {
-    //continue what it has been doing.
+
+  else if (flag_onoff ==false && flag_nav !='P') {
+    stop_move();
   }
 }
 
+
+
+
+
 /***********************************************************/
 // define functions
+void line_follow() {
+if (l2 == LOW && flag_nav != 'P')
+  {
+    Serial.println("Junctions detected!");
+    stop_move();
+  }
+
+  else if (l2 == HIGH)
+  {
+    // neither l0 or l1 detects the line
+    if ((l0 == HIGH && l1 == HIGH) && flag_nav != 'F')
+    {
+      // forward
+      move_forward();
+      Serial.println("MOVE FORWARD!");
+    }
+
+    // l0 detects the line
+    else if ((l0 == LOW && l1 == HIGH) && flag_nav != 'L')
+    {
+      // adjust left slightly
+      adjust_left();
+    }
+
+    // l1 detects the line
+    else if ((l0 == HIGH && l1 == LOW) && flag_nav != 'R')
+    {
+      // adjust right slightly
+      adjust_right();
+    }
+
+    // l0 and l1 both detect the line
+    else if ((l0 == LOW && l1 == LOW) && flag_nav != 'P')
+    {
+      // forward
+      stop_move();
+    }
+  }
+}
+
+// functions
+void move_forward()
+{
+  flag_nav = 'F';
+  Rwheel->run(FORWARD);
+  Rwheel->setSpeed(motor_speed);
+  Lwheel->run(FORWARD);
+  Lwheel->setSpeed(motor_speed);
+}
+
+void adjust_left()
+{
+  flag_nav = 'L';
+  Rwheel->run(FORWARD);
+  Rwheel->setSpeed(motor_speed);
+  Lwheel->run(BACKWARD);
+  Lwheel->setSpeed(200);
+}
+
+void adjust_right()
+{
+  flag_nav = 'R';
+  Rwheel->run(BACKWARD);
+  Rwheel->setSpeed(200);
+  Lwheel->run(FORWARD);
+  Lwheel->setSpeed(motor_speed);
+}
+
 void stop_move()
 {
-  flag_nav = "P";
+  flag_nav = 'P';
   Rwheel->run(RELEASE);
   Rwheel->setSpeed(0);
   Lwheel->run(RELEASE);
