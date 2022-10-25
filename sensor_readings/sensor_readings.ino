1a842f836c523ef93c42e248a9a83f11fb46c267
OrrEos <98087396+OrrEos@users.noreply.github.com>
2022-10-25 16:33:18 +0100

    edits to ultrasonic measure in sensor_readings

--- sensor_readings/sensor_readings.ino
+++ sensor_readings/sensor_readings.ino
@@ -1,40 +1,46 @@
 // //Analog:
-// const int ldr_pn = A0;
-// const int hall_pn = A1;
-// const int ir1_pn = A4; // short range
-// const int ir2_pn = A5; // long range
+const int ldr_pn = A0;
+const int hall_pn = A1;
+const int ir1_pn = A4; // short range
+const int ir2_pn = A5; // long range
 // //Digital:
-// const int button_pn=2; 
-// const int l0_pn=3; //left
-// const int l1_pn=4; //right
-// const int l2_pn=5; //far right (for juntion counting)
+const int button_pn=2; 
+const int l0_pn=3; //left
+const int l1_pn=4; //right
+const int l2_pn=5; //far right (for juntion counting)
 const int us1E_pn=6; // yellow wire
 const int us1T_pn=7; // green wire
-// const int ledG_pn=8;
-// const int ledA_pn=9;
-// const int ledR_pn=10;
+const int us2E_pn=12; // yellow
+const int us2T_pn=13; // green
+const int ledG_pn=8;
+const int ledA_pn=9;
+const int ledR_pn=10;
 // const int servo1_pn=11;
-// const int us2E_pn=12;
-// const int us2T_pn=13;
+
 // int motor_speed = 250;
 
 // // Sensor Values
-// int ldr, l0, l1, l2, l3, ir1, ir2, hall, push;
-float us1_distance, us2_distance;
-// float ir1_avg, ir2_avg, us1_avg, us2_avg;
+int ldr, l0, l1, l2, l3, ir1, ir2, hall, push;
+int us1_distance, us2_distance;
+float ir1_avg, ir2_avg;
 
-float us_measure(int trig_pin, int echo_pin)
+void us_measure()
 {
-  digitalWrite(trig_pin, LOW);
-  delayMicroseconds(2);
+  digitalWrite(us1T_pn, LOW);
+  digitalWrite(us2T_pn, LOW);
+  delayMicroseconds(1);
   // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trig_pin, HIGH);
+  digitalWrite(us1T_pn, HIGH);
+  digitalWrite(us2T_pn, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trig_pin, LOW);
+  digitalWrite(us1T_pn, LOW);
+  digitalWrite(us2T_pn, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  float duration_us = pulseIn(echo_pin, HIGH);
-  // return distance (*v_sound /2)
-  return duration_us * 0.017;
+  float us1_duration = pulseIn(us1E_pn, HIGH);
+  float us2_duration = pulseIn(us2E_pn, HIGH);
+  // get distance values(*v_sound /2)
+  us1_distance= us1_duration * 0.017;
+  us2_distance= us2_duration * 0.017; 
 }
 
 // void sensor_read()
@@ -69,9 +75,10 @@ void setup(){
 
 void loop(){
   // Serial.println("Test");
-  us1_distance = us_measure(us1T_pn, us1E_pn);
-  // us2_distance = us_measure(us2T_pn, us2E_pn);
+  us_measure();
   Serial.print("US1: ");
   Serial.println(us1_distance);
+  Serial.print("US2: ");
+  Serial.println(us2_distance);
 }
 
