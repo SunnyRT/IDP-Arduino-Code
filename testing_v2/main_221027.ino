6c9508383fb00811d443219b70ea0cddbcf8b5c2
OrrEos <98087396+OrrEos@users.noreply.github.com>
2022-10-27 14:48:58 +0100

    put pid tunnel into main

--- testing_v2/main_221027.ino
+++ testing_v2/main_221027.ino
@@ -26,8 +26,8 @@ const int l2_pn=4; //far right (for juntion counting)
 //us1: on the front
 const int us1E_pn=6; // yellow wire
 const int us1T_pn=7; // green wire
-const int ledG_pn=8;
-const int ledR_pn=9;
+const int ledG_pn=A2; // now analog
+const int ledR_pn=A3; // now analog
 const int ledA_pn=10;
 const int servo1_pn=11;
 
@@ -49,11 +49,30 @@ int ledAState = LOW;  // ledState used to set the LED
 unsigned long previousMillis = 0;  // will store last time LED was updated
 const long interval = 250;  // 2hz --> 0.5 second interval at which to blink (milliseconds)
 
-// flags
+// PID tunnel navigation variables
+int lastError = 0;
+float Kp = 0.07; // related to the proportional control term;
+// change the value by trial-and-error (ex: 0.07).
+float Ki = 0.0008; // related to the integral control term;
+// change the value by trial-and-error (ex: 0.0008).
+float Kd = 0.6; // related to the derivative control term;
+// change the value by trial-and-error (ex: 0.6).
+int P;
+int I;
+int D;
+const int maxspeedR = 150;
+const int maxspeedL = 150;
+const int basespeedR = 100;
+const int basespeedL = 100;
+
+
+/*************
+ * flags
+**************/
 int side; // 0,1,2,3,4,5
 int count = 0;
-bool flag_onoff = false;
-bool flag_started;
+bool flag_onoff = false; // push button: robot on or off
+bool flag_started;       // has robot completed initial start?
 bool flag_line;
 int flag_side; 
 char flag_nav; 
@@ -62,12 +81,14 @@ char flag_nav;
   //"R": adjust right
   //"B": backwards
   //"F": forwards
-bool flag_blk;
+bool flag_blk; // block collected
 bool flag_magnet;
+bool flag_tunnel; // true if in tunnel, false if not
 int box_intend;
 
-// declare functions
-
+/*************
+ * declare functions
+**************/
 void stop_move();
 void start_route();
 void move_forward();
@@ -94,7 +115,9 @@ void setup(){
   AFMS.begin(); // Connect to the motor controller
   servo_claw.attach(servo1_pn);  // connect to servo
 }
-
+/*************
+ * MAIN LOOP =======================================
+**************/
 void loop(){
     /*read & print line sensors*/ 
     Serial.print("Line Sensors: ");
@@ -111,7 +134,7 @@ void loop(){
     Serial.print("Flag Started: ");
     Serial.println(flag_started);
 
-    /* On-off Push Button:*/
+    /** On-off Push Button:*/
     // if the time elapsed is greater than time for debounce:
     if (millis() - lastTimeButtonStateChanged > debounceDuration){
         // read button state
@@ -134,7 +157,7 @@ void loop(){
     Serial.print("ONOFF: ");
     Serial.println(flag_onoff);
     
-    /*Navigation*/
+    /** Navigation */
     // if 'Off' and not already stopped, stop:
     if(flag_onoff==false && flag_nav != 'P'){
         stop_move();
@@ -146,76 +169,126 @@ void loop(){
     // if On and the start fn has been completed:
     if(flag_onoff==true && flag_started==true){
 
-        // front sensor detects something (hopefully block!)
+        /** Block Detection*/
         if(us1_distance < 10){
+            
+            // approach slowly until us1_distance = 2cm 
             stop_move();
             Serial.println("Block Detected");
             delay(1000);
+            Serial.println("Start detecting if magnet");
             /* blk function */
             if (hall == 0){
                 // magnet
-                flag_magnet
+                Serial.println("Magnet");
+                flag_magnet = 1;
+                box_intend = 3; // blk is to be delivered in the red box
+                // RED LED for 5sec
+                analogWrite(ledR_pn, 255);
+                delay(5000);
+                analogWrite(ledR_pn, 0);
             }
-        }
-
-        /* line-follow or side-sensor navigation ?*/
-        // if side (*RIGHT*) sensor (us2) < 6cm (or so), assume in tunnel
-        if(us2_distance < 6){
-            /* tunnel navigation */
-            Serial.println("Tunnel: Distance Navigation");
-            // too close to right wall (move left slightly)
-            if(us2_distance <=2){
-                adjust_left();
-            }
-            // too far from right wall (move right slightly)
-            if(us2_distance >=10){
-                adjust_right();
-            }
-        }
-
-        else { /* line follow */
-
-            // xx0 (if far-right sensor has no line)
-            if(l2==LOW){
-
-                // 000 & it's not already moving forward
-                if((l0 == LOW && l1 == LOW) && flag_nav != 'F'){
-                    move_forward();
-                    Serial.println("Move Forward");
-                }
-
-                // 100 & not already moving left
-                else if((l0 == HIGH && l1 == LOW) && flag_nav != 'L'){
-                    adjust_left();
-                }
-
-                // 010 & not already moving right
-                else if ((l0 == LOW && l1 == HIGH) && flag_nav != 'R'){
-                    adjust_right();
-                    }
-            }
-
-            // xx1 or 11x(junction detected)
-            else if(l2==HIGH || (l0==HIGH && l1==HIGH)){
-                // handle junction depending on flags
-                Serial.println("Junction Detected");
-            }
-
-            // catch all other cases: stop & report error
             else{
-                stop_move();
-                Serial.print("Line Sensor Error: ");
-                Serial.print(l0);
-                Serial.print(l1);
-                Serial.println(l2);
+                // no magnet
+                Serial.println("Not Magnet");
+                flag_magnet = 0;
+                box_intend = 1; // bl
+                // Green LED for 5sec
+                analogWrite(ledG_pn, 255);
+                delay(5000);
+                analogWrite(ledG_pn, 0);
             }
+
+            /** claw  code */
+
+            flag_blk = true;
         }
-    
-    }
+
+        /** No Block Detected:*/
+        else {
+            /* line-follow or side-sensor navigation ?*/
+            // if (*RIGHT*) sensor (us2) < 6cm, entering tunnel
+            if((us2_distance < 6)){
+                flag_tunnel = true;
+                /** PID tunnel navigation */
+                int error = 5.0 - us2_distance; // 5.0 is the ideal distance from the tunnel wall
+                P = error;
+                I = I + error;
+                D = error - lastError;
+                lastError = error;
+                int motorspeed = P * Kp + I * Ki + D * Kd; // calculate the correction
+                // needed to be applied to the speed
+                int motorspeedR = basespeedR + motorspeed;
+                int motorspeedL = basespeedL - motorspeed;
+
+                if (motorspeedR > maxspeedR){
+                    motorspeedR = maxspeedR;
+                }
+                if (motorspeedL > maxspeedL){
+                    motorspeedL = maxspeedL;
+                }
+                if (motorspeedR < 0){
+                    motorspeedR = 0;
+                }
+                if (motorspeedL < 0){
+                    motorspeedL = 0;
+                }
+
+                flag_nav = 'F';
+                Rwheel->run(FORWARD);
+                Rwheel->setSpeed(motorspeedR);
+                Serial.print("speedR: ");
+                Serial.println(motorspeedR);
+
+                Lwheel->run(FORWARD);
+                Lwheel->setSpeed(motorspeedL);
+                Serial.print("speedL: ");
+                Serial.println(motorspeedL);
+            }
+
+            else { /* line follow */
+
+                // xx0 (if far-right sensor has no line)
+                if(l2==LOW){
+
+                    // 000 & it's not already moving forward
+                    if((l0 == LOW && l1 == LOW) && flag_nav != 'F'){
+                        move_forward();
+                        Serial.println("Move Forward");
+                    }
+
+                    // 100 & not already moving left
+                    else if((l0 == HIGH && l1 == LOW) && flag_nav != 'L'){
+                        adjust_left();
+                    }
+
+                    // 010 & not already moving right
+                    else if ((l0 == LOW && l1 == HIGH) && flag_nav != 'R'){
+                        adjust_right();
+                    }
+                }
+
+                // xx1 or 11x(junction detected)
+                else if(l2==HIGH || (l0==HIGH && l1==HIGH)){
+                    // handle junction depending on flags
+                    Serial.println("Junction Detected");
+                }
+
+                // catch all other cases: stop & report error
+                else{
+                    stop_move();
+                    Serial.print("Line Sensor Error: ");
+                    Serial.print(l0);
+                    Serial.print(l1);
+                    Serial.println(l2);
+                }
+            } // close line follow
+        } // close if nothing detected in front
+    } // close 'On' 
  }// close loop
 
 
-
+/*========= FUNCTIONS ===================*/
 void start_route(){   
   // if still in the box (so no line detected), move forward until edge of box detected
     if (l0 == LOW && l1 == LOW && l2 == LOW && flag_nav != 'F')
